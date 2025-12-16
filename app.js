add4c4bf843bb16b4bca14f44f2dcc6d30bd1e3d
luvugk2-cloud <luvugk2@gmail.com>
2025-12-16 18:19:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -50,11 +50,11 @@ app.post('/formsg/webhook',
             console.log(submission);
             ////////////////mapping logic///////////////////
             // 2) Extract FormSG responses
-  //const body = req.body;
-  //const responses = body?.data?.responses || [];
+  const body = req.body;
+  const responses = body?.data?.responses || [];
 
   function findField(name) {
-    const f = submission.find((r) =>
+    const f = responses.find((r) =>
       r.question.toLowerCase().includes(name.toLowerCase())
     );
     return f?.answer || null;
